aec10c210d70e4c173eddff655425995c9a88516
brucejh99 <bjhong@yahoo.com>
2018-09-21 23:35:13 -0400

    Updated UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const fbLogo = require('./assets/fb-logo.svg');
+const gmailLogo = require('./assets/gmail-logo.svg');
+
+const notificationIcon = require('./assets/notification-icon.png');
+const messageIcon = require('./assets/message-icon.png');
+
 class App extends Component {
   render() {
     return (
@@ -10,15 +16,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Notification Hub</h1>
         </header>
-        <MediaBox icon={logo} mediaName="Name 1"/>
-        <MediaBox icon={logo} mediaName="Name 2"/>
-        <MediaBox icon={logo} mediaName="These are all classes"/>
+        <NotifMsgMediaBox icon={fbLogo} mediaName="Facebook"/>
+        <NotifMediaBox icon={gmailLogo} mediaName="Gmail"/>
+        <NotifMediaBox icon={logo} mediaName="These are all classes"/>
       </div>
     );
   }
 }
 
-class MediaBox extends Component {
+class NotifMediaBox extends Component {
   constructor(props) {
     super(props);
     this.logo = props.icon;
@@ -29,6 +35,25 @@ class MediaBox extends Component {
     <div className="media-box">
       <img src={this.logo} className="media-icon" alt="logo1"/>
       <p className="media-name">{this.mediaName}</p>
+      <img src={notificationIcon} className="notification-icon" alt="logo1"/>
+    </div>
+    );
+  }
+}
+
+class NotifMsgMediaBox extends Component {
+  constructor(props) {
+    super(props);
+    this.logo = props.icon;
+    this.mediaName = props.mediaName;
+  }
+  render() {
+    return (
+    <div className="media-box">
+      <img src={this.logo} className="media-icon" alt="logo1"/>
+      <p className="media-name">{this.mediaName}</p>
+      <img src={messageIcon} className="notification-icon" alt="logo1"/>
+      <img src={notificationIcon} className="notification-icon" alt="logo1"/>
     </div>
     );
   }
