4a95d0fb03942583fec53018fa0da8ec4b567662
brucejh99 <bjhong@yahoo.com>
2018-09-30 22:19:24 -0400

    Merge customization bar with app.js for global state/page monitoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CustomizeBar from './components/CustomizeBar';
 
 import Home from './components/Home';
 // import Settings from './components/Settings';
 // import Customize from './components/AddRemoveMedia';
+
+const homeIcon = require('./assets/home-icon.png')
+const addIcon = require('./assets/add-icon.png');
+const settingsIcon = require('./assets/settings-icon.png');
 
 export default class App extends Component {
   constructor() {
@@ -16,23 +19,45 @@ export default class App extends Component {
       customize: false,
     }
   }
+
+  currentPage = <Home />;
   
   clickHandler() {
     if(this.state.home) {
-      // this.currentPage = Home;
+      this.currentPage = <Home />;
     } else if(this.state.settings) {
-      // this.currentPage = Settings;
+      // this.currentPage = <Settings />;
     } else {
-      // this.currentPage = Customize;
+      // this.currentPage = <Customize />;
     }
   }
 
   render() {
     return (
       <div>
-        <CustomizeBar />
-        <Home />
+        <div className="customize-bar">
+          <CustomizeButton icon={homeIcon} handleClick={ () => console.log('Home') } />
+          <CustomizeButton icon={addIcon} handleClick={ () => console.log('Add social media') } />
+          <CustomizeButton icon={settingsIcon} handleClick={ () => console.log('Settings') } />
+        </div>
+        {this.currentPage}
       </div>
     );
+  }
+}
+
+class CustomizeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.icon = props.icon;
+    this.handleClick = props.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <button className="customize-button" onClick={this.handleClick}>
+          <img src={this.icon} className="customize-button" alt="add-icon" />
+      </button>
+    )
   }
 }
