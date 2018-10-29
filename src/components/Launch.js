c829534768ffd35bc3bfce0b51c8eb28674ec372
brucejh99 <bjhong@yahoo.com>
2018-10-29 00:03:51 -0400

    Created layout for loading page

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './Launch.css';
 
 export default class Launch extends Component {
+  constructor(props) {
+    super(props);
+    this.value = props.value;
+    this.updateName = props.updateName;
+    this.MLA = false;
+    this.APA = false;
+    this.Chicago = false;
+    this.Harvard = false;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">inCite</h1>
-        </header>
-
+      <div>
+        <h1>inCite</h1>
+        <div className="input-box">
+            <input className="input-field" value={this.value} onKeyPress={this.updateName} type="text" placeholder="Bibliography Name" />
+        </div>
+        <div className="button-container space-between">
+            <StyleButton styleName="MLA" selected={this.MLA}/>
+            <StyleButton styleName="APA" selected={this.APA}/>
+            <StyleButton styleName="Chicago" selected={this.Chicago}/>
+            <StyleButton styleName="Harvard" selected={this.Harvard}/>
+        </div>
       </div>
     );
+  }
+}
+
+class StyleButton extends Component {
+  constructor(props) {
+    super(props);
+    this.selected = props.selected;
+    this.styleName = props.styleName;
+  }
+  
+  render() {
+    return (
+      <div>
+          <button className={this.selected ? "style-button-selected" : "style-button-default"}>
+            {this.styleName}
+          </button>
+      </div>
+    )
   }
 }
