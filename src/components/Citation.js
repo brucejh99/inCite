bfde1ce45890866982475f2a8bb730885a0a7855
ddlan <L.danny18725@gmail.com>
2018-10-29 00:04:32 -0400

    layout of citation page

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -7,10 +7,53 @@ export default class Citation extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">inCite</h1>
+          <h1>Format: MLA</h1>
         </header>
 
+        <div className="body">
+          <form>
+            <div className="table">
+              <label className="tr">
+                <span className="td table-name">Website Name</span>
+                <span className="td table-field">
+                  <input className="name-input" type="text" name="website-name"
+                    placeholder="Website Name"/>
+                </span>
+              </label>
+              <label className="tr">
+                <span className="td table-name">Article Name</span>
+                <span className="td table-field">
+                  <input className="name-input" type="text" name="article-name"
+                    placeholder="Article Name"/>
+                </span>
+              </label>
+              <label className="tr">
+                <span className="td table-name">Author</span>
+                <span className="td table-field">
+                  <input className="author-name-input author-first-name" type="text"
+                    name="author-first-name" placeholder="First Name"/>
+                  <input className="author-name-input" type="text"
+                    name="author-last-name" placeholder="Last Name"/>
+                </span>
+              </label>
+              <label className="tr">
+                <span className="td table-name">Date Published</span>
+                <span className="td table-field">
+                  <input className="name-input" type="date"
+                    name="date-published" placeholder="Date Published"/>
+                </span>
+              </label>
+              <label className="tr">
+                <span className="td table-name">Sponsor</span>
+                <span className="td table-field">
+                  <input className="name-input" type="text"
+                    name="sponsor" placeholder="Sponsor"/>
+                </span>
+              </label>
+            </div>
+            <div className="add-citation"><input type="submit" value="Add Citation" /></div>
+          </form>
+        </div>
       </div>
     );
   }
