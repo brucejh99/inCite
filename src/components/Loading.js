7a5d4c645933773ad9ce270831c760c322ba4d9f
brucejh99 <bjhong@yahoo.com>
2018-12-11 04:07:59 -0500

    Fixed css path and naming

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
+import './Loading.css';
 
 export default class LoadingPage extends Component {
   render() {
     return (
-      <div>
+      <div className="body">
         <header>
-          <img src={logo} className="App-logo" />
-          <h1 className="App-title">Pulling data...</h1>
+          <img src={logo} className="load-image" />
+          <h1>Pulling data...</h1>
         </header>
       </div>
     );
