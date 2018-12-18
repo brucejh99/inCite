ecdf08e160f7fb3d11ac4875e6dbe875a410390b
brucejh99 <bjhong@yahoo.com>
2018-12-17 19:07:12 -0500

    Created copy to clipboard template

--- src/components/Bibliography.js
+++ src/components/Bibliography.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Bibliography.css';
 import { getOrSetBibliography, resetBibliography } from '../services/Storage';
+import { toAPA, toMLA, toChicago, toHarvard } from '../services/Converter';
 
 export default class Bibliography extends Component {
   constructor(props) {
@@ -11,6 +12,8 @@ export default class Bibliography extends Component {
     }
   }
 
+  // TODO: loop through bibliography array and convert based on type
+
   render() {
     return (
       <div className="body">
@@ -18,8 +21,47 @@ export default class Bibliography extends Component {
           <h1 className="title">Bibliography</h1>
         </header>
         <body>
-
+          <CopyClipboard bibliography={this.state.bibliography} />
         </body>
+      </div>
+    );
+  }
+}
+
+// TODO: fix so that copy doesn't copy HTML itself
+
+class CopyClipboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bibliography: props.bibliography,
+      copySuccess: ''
+    }
+  }
+
+  copyToClipboard = (e) => {
+    this.textArea.select();
+    document.execCommand('copy');
+    e.target.focus();
+    this.setState({ copySuccess: 'Copied!' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <textarea
+            ref={(textarea) => this.textArea = textarea}
+            value={this.state.bibliography}
+          />
+        </form>
+        {
+         document.queryCommandSupported('copy') &&
+          <div>
+            <button onClick={this.copyToClipboard}>Copy</button> 
+            {this.state.copySuccess}
+          </div>
+        }
       </div>
     );
   }
