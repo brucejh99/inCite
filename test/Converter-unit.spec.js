1c7e9958d82a24d4b0db3e7b1585135a6881bce6
brucejh99 <bjhong@yahoo.com>
2018-12-21 19:36:58 -0500

    Beginning work on unit tests

---
+++ test/Converter-unit.spec.js
@@ -0,0 +1,61 @@
+import { toAPA, toMLA, toChicago, toHarvard } from '../src/services/Converter';
+import chai from 'chai';
+
+chai.should();
+
+function makeData(title, author, date, publisher, url, dateRetrieved, success) {
+    var metadata = {};
+    if(title) metadata.title = 'Title';
+    if(author) metadata.author = 'FirstName MiddleName LastName';
+    if(date) metadata.date = '2017-12-21T15:00:01.069Z';
+    if(publisher) metadata.publisher = 'Publisher';
+    if(url) metadata.url = 'www.url.com/test/';
+    if(dateRetrieved) metadata.dateRetrieved = new Date('2018-12-25T15:00:01.069Z');
+    metadata.success = success;
+    return metadata;
+}
+
+describe('Unit tests for APA converter', function() {
+    it('Should generate correct citations with full metadata', function() {
+        const metadata = makeData(true, true, true, true, true, true, true);
+        const expectedResult = 'LastName, F. M. (2017, December 21). <i>Title</i>. Publisher. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    it('Should generate correct citations with no title', function() {
+        const metadata = makeData(false, true, true, true, true, true, true);
+        const expectedResult = 'LastName, F. M. (2017, December 21). Publisher. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    it('Should generate correct citations with no author', function() {
+        const metadata = makeData(true, false, true, true, true, true, true);
+        const expectedResult = '<i>Title</i>. (2017, December 21). Publisher. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    it('Should generate correct citations with no date published', function() {
+        const metadata = makeData(true, true, false, true, true, true, true);
+        const expectedResult = 'LastName, F. M. <i>Title</i>. Publisher. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    it('Should generate correct citations with no publisher', function() {
+        const metadata = makeData(true, true, true, false, true, true, true);
+        const expectedResult = 'LastName, F. M. (2017, December 21). <i>Title</i>. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    xit('Should do something with no url', function() {
+        const metadata = makeData(true, true, true, true, false, true, true);
+        const expectedResult = 'LastName, F. M. (2017, December 21). <i>Title</i>. Retrieved (2018, December 25), from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+    it('Should generate correct citations with no date retrieved', function() {
+        const metadata = makeData(true, true, true, true, true, false, true);
+        const expectedResult = 'LastName, F. M. (2017, December 21). <i>Title</i>. Publisher. Retrieved from www.url.com/test/.';
+        const citation = toAPA(metadata);
+        citation.should.equal(expectedResult);
+    });
+});
