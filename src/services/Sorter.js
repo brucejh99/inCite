d1e1fc5b22c89993117dd49f53de011ec93fc6f4
brucejh99 <bjhong@yahoo.com>
2019-01-01 17:44:15 -0500

    Completed APA sorter

--- src/services/Sorter.js
+++ src/services/Sorter.js
@@ -7,16 +7,36 @@ function getLastName(author) {
     }
 }
 
+/**
+ * Function to be passed into an array of citation objects' sort() method to sort it into APA order
+ * @param {Object} citation1 
+ * @param {Object} citation2 
+ */
 export function APASort(citation1, citation2) {
-    let sortVal;
     if(citation1.author && citation2.author) {
-        const lastName1 = getLastName(citation1);
+        const lastName1 = getLastName(citation1.author);
+        const lastName2 = getLastName(citation2.author);
+        if(lastName1 < lastName2) return 1;
+        else if(lastName1 > lastName2) return -1;
+        else return 0;
     } else if(citation1.author) {
-        // conditions
+        const lastName1 = getLastName(citation1.author);
+        const title2 = citation2.title || '';
+        if(lastName1 < title2) return 1;
+        else if(lastName1 > title2) return -1;
+        else return 0;
     } else if(citation2.author) {
-        // conditions
+        const title1 = citation1.title || '';
+        const lastName2 = getLastName(citation2.author);
+        if(title1 < lastName2) return 1;
+        else if(title1 > lastName2) return -1;
+        else return 0;
     } else {
-        // title only conditions
+        const title1 = citation1.title || '';
+        const title2 = citation2.title || '';
+        if(title1 < title2) return 1;
+        else if(title2 > title2) return -1;
+        else return 0;
     }
 }
 
