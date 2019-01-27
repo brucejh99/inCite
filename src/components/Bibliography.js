d148d95fb520714b80e6fbde1bd72baafe3e9ebe
brucejh99 <bjhong@yahoo.com>
2019-01-26 23:16:09 -0500

    Modified lazy load and styles to be smoother

--- src/components/Bibliography.js
+++ src/components/Bibliography.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Bibliography.css';
 import { Button, List, ListItem, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { getOrSetBibliography, resetBibliography, updateBibliography } from '../services/Storage';
 import { toAPA, toMLA, toChicago, toHarvard } from '../services/Converter';
 import { APASort, MLASort, ChicagoSort, HarvardSort } from  '../services/Sorter';
