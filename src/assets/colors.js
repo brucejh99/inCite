a193ba411cdecc1238810627fc1d4924cb3123f9
brucejh99 <bjhong@yahoo.com>
2019-06-07 01:10:07 -0400

    Added global colour constants file

---
+++ src/assets/colors.js
@@ -0,0 +1,11 @@
+// background theme for light mode
+export const LIGHT_THEME = '#ffffff';
+// background theme for dark mode
+export const DARK_THEME = '#363535';
+
+// red used on most buttons on the bottom on the pages
+export const RED = '#F69970';
+// orange used on intro page buttons and input field
+export const ORANGE = '#C3790B';
+// standard yellow used on most pages
+export const YELLOW = '#FFD600';
