4cd3d3d1a79b1d22b0048a710d90249c8a5e09a9
brucejh99 <bjhong@yahoo.com>
2019-06-15 03:59:49 -0400

    Added bolder Nunito Sans file

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,10 +2,14 @@
 export const LIGHT_THEME = '#ffffff';
 // background theme for dark mode
 export const DARK_THEME = '#363535';
+// gradient background on home page
+export const GRADIENT = 'linear-gradient(191.76deg, rgba(255, 168, 0, 0) 15.19%, rgba(232, 39, 186, 0.615317) 104.2%, #8F00FF 152.47%), #FFE455';
 
 // red used on most buttons on the bottom on the pages
 export const RED = '#F69970';
 // orange used on intro page buttons and input field
-export const ORANGE = '#C3790B';
+export const ORANGE = '#C3790B'
+export const ORANGE_TRANSPARENT = '#C3790B69';
 // standard yellow used on most pages
 export const YELLOW = '#FFD600';
+export const LIGHT_YELLOW = '#fff1aa';
